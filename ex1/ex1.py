6a370181aef74aca8c39da73f6d825fa58da3cdb
amin <ahmadi.amin.a@gmail.com>
2018-04-09 22:44:54 +0200

    Assignment #2 first part

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -4,7 +4,6 @@
 # Date: Jan 12, 2018
 ############################################################
 import numpy as np
-import matplotlib.pyplot as pl
 
 ############################################################
 
@@ -22,9 +21,12 @@ def J_cost(X,Y, theta):
 ############################################################
 def g_desc(X,Y, theta, alpha, num_iters):
     """ This function uses gradient descent to fit the 
-    hypothesis parameters"""
+    hypothesis parameters
+    """
     m = Y.shape[0]
+    # value of cost function is saved for plotting
     J_arr = np.zeros((num_iters), float)
+    theta_arr = np.zeros((num_iters,2), float)
     
     for it in range(num_iters):
         error = np.dot(X,theta) - Y
@@ -33,9 +35,11 @@ def g_desc(X,Y, theta, alpha, num_iters):
         # update theta
         theta = np.array([aux0, aux1], float)
 
+        # save data for plotting
+        theta_arr[it] = theta
         J_arr[it] = J_cost(X,Y,theta)
 
-    return theta
+    return theta, J_arr, theta_arr
 
 ############################################################
 # 1 generate a identity matrix of size 5x5
@@ -47,7 +51,7 @@ A = np.eye(5, dtype=float)
 # 2nd column: profit
 data = np.loadtxt('ex1data1.txt', dtype=float, delimiter=',')
 X = data[:,0]; Y = data[:,1]    # feature and goal
-print X.shape
+print(X.shape)
 m = X.shape[0]                     # number of training example
 
 ############################################################
@@ -57,15 +61,31 @@ X = np.c_[np.ones(m), X] # add a column of one to X
 theta = np.zeros(2)    # initialize fitting parameter
 num_iters = 1500
 alpha = 0.01
+J_arr = np.zeros((num_iters), float)
+theta_arr = np.zeros((num_iters,2), float)
 
-print "Cost: ", J_cost(X,Y,theta)
-theta[0], theta[1] =  g_desc(X,Y, theta, alpha, num_iters)
+print("Cost: ", J_cost(X,Y,theta))
+theta, J_arr, theta_arr =  g_desc(X,Y, theta, alpha, num_iters)
 
-print "theta = ", theta
-# plot data 
-pl.plot(data[:,0], data[:,1], '*k')
-pl.plot(data[:,0], theta[0] + theta[1]*data[:,0], 'b-')
-pl.xlabel('Population of City in 10,000s')
-pl.ylabel('Profit in $10,000a')
-pl.xlim(4,)
+print( "theta = ", theta)
+
+##################################################
+###                 plot data                  ###
+##################################################
+import matplotlib.pyplot as pl
+
+fig = pl.figure(figsize=(12,6))
+ax = fig.add_subplot(1,2,1)
+ax.plot(data[:,0], data[:,1], '*k')
+ax.plot(data[:,0], theta[0] + theta[1]*data[:,0], 'b-')
+ax.set_xlabel('Population of City in 10,000s')
+ax.set_ylabel('Profit in $10,000a')
+ax.set_xlim(4,)
+
+ax1 = fig.add_subplot(1,2,2)
+ax1.plot(np.linspace(0,1,num_iters), theta_arr[:,0] )
+ax1.plot(np.linspace(0,1,num_iters),theta_arr[:,1])
+
+# ax1.contour(J_arr, theta_arr[:,0], theta_arr[:,1])
+
 pl.show()
---
+++ ex2/ML_ex2.py
@@ -0,0 +1,142 @@
+# Machine Learning courseby Andrew Ng
+# Python version
+# Solution by: Amin Ahmadi
+# Date: April 9, 2018
+
+#### Logistic regression, classification
+##################################################
+import numpy as np
+import matplotlib.pyplot as pl
+
+# Read admission data into an numpy array for analysis and plotting
+data = np.loadtxt('ex2data1.txt', delimiter=',', dtype=float)
+pl.plot(data[:,0], data[:,1], '*b')
+
+# To find admitted and rejected student
+
+#admitted students
+dp = data[data[:,2]==1,0:2]
+#rejected students
+dn = data[data[:,2]==0,0:2]
+
+
+# Plot admitted and rejected student with different colors
+pl.plot(dp[:,0], dp[:,1], 'ko', label='admitted')
+pl.plot(dn[:,0], dn[:,1], 'b*', label='rejected')
+pl.legend()
+
+########################################
+def sigmoid(x):
+    """Calculate sigmoid function of input paramter.
+    input:
+    ------
+    x: float
+    
+    return:
+    -------
+    aux: float
+    """
+    if x == 0:
+        aux=0.5
+    else:
+        aux = 1./( 1+np.exp(-x) )
+        
+    return aux
+# to be applicable to an array, vectorize it 
+sigmoid = np.vectorize(sigmoid)
+
+########################################
+def costFun(x,y,theta):
+    """Calculate the cost function in logistic regression.
+    input:
+    ------
+    x: array, float, features
+    y: array, float, targets
+    theta: array, float, hypothesis parameter
+    
+    return:
+    -------
+    float, the cost 
+    """
+    aux = 0
+    M = x.shape[0]
+    aux = np.sum(-y*np.log(sigmoid(np.dot(x,theta))) -\
+                 (1-y)*np.log(1-sigmoid(np.dot(x,theta))))
+    return aux/M
+
+########################################
+def costGrad(x,y,theta):
+    """Gradient of the logistic regression cost function.
+    input:
+    ------
+    x: array, float, features
+    y: array, float, target
+    theta: array, hypothesis paramters
+    
+    return:
+    -------
+    grad: array, float, dimension of features
+    """
+    M = x.shape[0]
+    grad = np.zeros((x.shape[1]),float)
+    temp = sigmoid(np.dot(theta,x.T))
+    error = temp - y
+    grad = (1./M)*np.dot(x.T,error)
+    return grad
+
+########################################
+def g_des(x,y,theta_int,alpha):
+    """Gradient desendent to find the minimum of the cost function.
+    input:
+    ------
+    x: array, float, features
+    y: array, float, target
+    theta_int: array, hypothesis paramters, initial guess.
+    
+    return:
+    -------
+    theta:array, hypothesis parameters, fitted
+    Jarr: array, cost function in each iteration
+    """
+    num_itr = 5000
+    Jarr = np.zeros((num_itr), float)
+    print(Jarr.shape)
+    for it in range(num_itr):
+        theta -= alpha*costGrad(x,y,theta)
+        Jarr[it] = costFun(x,y,theta)
+    return theta, Jarr
+
+########################################
+
+x = data[:,0:2]
+m = x.shape[0]
+# add one column of 1s to the featurs for theta_0
+x = np.hstack((np.ones((m,1), float),x))
+y = data[:,2]
+theta_int = np.array([-10, 0.1,0.1])
+theta_f, J_arr = g_des(x,y,theta_int,0.001)
+print(theta_f)
+print(sigmoid(np.dot(theta_f,np.array([1,45,85],float))))
+d1 = np.linspace(0,1,5000)
+pl.plot(d1,J_arr, 'k,')
+
+
+########################################
+from scipy.optimize import minimize
+# define one-paramter cost function for optimization
+def cost_1param(theta):
+    """one parameter cost function for optimization"""
+    x = data[:,0:2]
+    m = x.shape[0]
+    x1 = np.ones((m,1),float)
+    x = np.hstack((x1,x))
+    y = data[:,2]
+    return costFun(x,y,theta)
+
+#optimization using "minimize" from scipy
+res = minimize(cost_1param,(0.,0.1,0.1))
+print(res)
+print(res.x)
+#prediction for test of (45,85) = 0.776
+sigmoid(np.dot(res.x,np.array([1,45,85])))
+
